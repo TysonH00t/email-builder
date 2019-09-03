c92439f6c4a5d17c99abc5e552c754f5f16d7bc9
TysonH00t <37850289+TysonH00t@users.noreply.github.com>
2019-09-03 14:46:56 -0700

    Added more to the ConvertToHTML

--- src/Components/ConvertToHTML/ConvertToHTML.js
+++ src/Components/ConvertToHTML/ConvertToHTML.js
@@ -1,88 +1,234 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 import { convertToHTML } from "draft-convert";
 
 class ConvertToHTML extends Component {
-    render() { 
-        return ( 
-            <div>
-                {this.props.Secs.map((section,Sindex) => (
-                    this.props.Secs[Sindex].content.map((content,Cindex) => (
-          
-                      this.props.Secs[Sindex].content[Cindex].content.editorState == undefined ? null : 
-                        <p>
-                      { convertToHTML({
-                        styleToHTML: style => {
-                          // if (style === "BOLD") {
-                          //   return <strong />;
-                          // }
-                          // if (style === "ITALIC") {
-                          //   return <em />;
-                          // }
-                          // if (style === "UNDERLINE") {
-                          //   return <span style={{ textDecoration: 'underline' }} />;
-                          // }
-                          if (style === "blue") {
-                            return <span style={{ color: '#0078D7' }} />;
-                          }
-                          if (style === "white") {
-                            return <span style={{ color: 'white' }} />;
-                          }
-                        },
-                        blockToHTML: block => {
-                          console.log('block' + block.type)
-                          if (block.type === "PARAGRAPH") {
-                            console.log('paragraph')
-                            return { element: <p />, empty: '' };
-                          }
-                          // if (block.type === "H1") {
-                          //   return { element: <h1 />, empty: <br /> };
-                          // }
-                          // if (block.type === "H2") {
-                          //   return { element: <h2 />, empty: <br /> };
-                          // }
-                          // if (block.type === "H3") {
-                          //   return { element: <h3 />, empty: <br /> };
-                          // }
-                          if (block.type === "unstyled") {
-                            return {
-                              start: '<p>',
-                              end: '</p>'
-                            };
-                          }
-                          if (block.type === "atomic") {
-                            return {
-                              start: "",
-                              end: ""
-                            };
-                          }
-                        },
-                        entityToHTML: (entity, originalText) => {
-                          console.log(entity)
-                          if (entity.type === "LINK") {
-                            return <a href={entity.data.url}>{originalText}</a>;
-                          }
-                          if (entity.type === "image") {
-                            return <img alt="" src={entity.data.src} />;
-                          }
-                          return originalText;
-                        }
-                      })(this.props.Secs[Sindex].content[Cindex].content.editorState.getCurrentContent())} </p>
-                    ))
-                    
-                    ))}
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div>
+        &lt;!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd"&gt; &lt;html&gt; &lt;head&gt;
+        &lt;meta name="viewport" content="width=device-width, initial-scale=1,
+        maximum-scale=1, user-scalable=0" /&gt; &lt;meta
+        http-equiv="Content-Type" content="text/html; charset=UTF-8" /&gt;
+        &lt;style&gt; /* Font Definitions */ @font-face &#x7b; font-family:
+        "Cambria Math"; panose-1: 2 4 5 3 5 4 6 3 2 4; mso-font-charset: 0;
+        mso-generic-font-family: roman; mso-font-pitch: variable;
+        mso-font-signature: 3 0 0 0 1 0; &#x7d; @font-face &#x7b; font-family:
+        Calibri; panose-1: 2 15 5 2 2 2 4 3 2 4; mso-font-charset: 0;
+        mso-generic-font-family: swiss; mso-font-pitch: variable;
+        mso-font-signature: -536859905 -1073732485 9 0 511 0; &#x7d; @font-face
+        &#x7b; font-family: "Segoe UI"; panose-1: 2 11 5 2 4 2 4 2 2 3;
+        mso-font-charset: 0; mso-generic-font-family: swiss; mso-font-pitch:
+        variable; mso-font-signature: -469750017 -1073683329 9 0 511 0; &#x7d;
+        @font-face &#x7b; font-family: "Segoe UI Light"; panose-1: 2 11 5 2 4 2
+        4 2 2 3; mso-font-charset: 0; mso-generic-font-family: swiss;
+        mso-font-pitch: variable; mso-font-signature: -469750017 -1073683329 9 0
+        511 0; &#x7d; /* Style Definitions */ /* @media screen and
+        (max-width:600px) &#x7b; table &#x7b; width: 100% !important; border: 0
+        !important; &#x7d; td &#x7b; width: 100% !important; &#x7d; .main td
+        &#x7b; width: 100% !important; height: auto !important; padding-top: 0px
+        !important; &#x7d; .mainHead td &#x7b; display: block !important; &#x7d;
+        .mainHead img &#x7b; height: auto !important; max-width: 640px
+        !important; width: 100% !important; &#x7d; .borderRight &#x7b;
+        border-right: #0078D7 10px solid !important; &#x7d; &#x7d; */
+        p.MsoNormal, li.MsoNormal, div.MsoNormal &#x7b; mso-style-unhide: no;
+        mso-style-qformat: yes; mso-style-parent: ""; margin: 0in;
+        mso-pagination: widow-orphan; font-size: 12.0pt; font-family: "Times New
+        Roman", serif; mso-fareast-font-family: Calibri; mso-fareast-theme-font:
+        minor-latin; &#x7d; a:link, span.MsoHyperlink &#x7b; mso-style-noshow:
+        yes; mso-style-priority: 99; color: #0078D1; text-decoration: underline;
+        text-underline: single; &#x7d; a:visited, span.MsoHyperlinkFollowed
+        &#x7b; mso-style-noshow: yes; mso-style-priority: 99; color: #0078D1;
+        text-decoration: underline; text-underline: single; &#x7d; p &#x7b;
+        mso-style-noshow: yes; mso-style-priority: 99; mso-margin-top-alt: auto;
+        margin-right: 0in; mso-margin-bottom-alt: auto; margin-left: 0in;
+        mso-pagination: widow-orphan; font-size: 12.0pt; font-family: "Times New
+        Roman", serif; mso-fareast-font-family: Calibri; mso-fareast-theme-font:
+        minor-latin; &#x7d; p.msonormal0, li.msonormal0, div.msonormal0 &#x7b;
+        mso-style-name: msonormal; mso-style-noshow: yes; mso-style-priority:
+        99; mso-style-unhide: no; mso-margin-top-alt: auto; margin-right: 0in;
+        mso-margin-bottom-alt: auto; margin-left: 0in; mso-pagination:
+        widow-orphan; font-size: 12.0pt; font-family: "Times New Roman", serif;
+        mso-fareast-font-family: Calibri; mso-fareast-theme-font: minor-latin;
+        &#x7d; span.EmailStyle19 &#x7b; mso-style-type: personal-compose;
+        mso-style-noshow: yes; mso-style-unhide: no; font-family: "Times New
+        Roman", serif; mso-ascii-font-family: "Times New Roman";
+        mso-fareast-font-family: "Times New Roman"; mso-hansi-font-family:
+        "Times New Roman"; mso-bidi-font-family: "Times New Roman";
+        mso-no-proof: yes; &#x7d; .MsoChpDefault &#x7b; mso-style-type:
+        export-only; mso-default-props: yes; font-size: 10.0pt;
+        mso-ansi-font-size: 10.0pt; mso-bidi-font-size: 10.0pt; &#x7d; @page
+        WordSection1 &#x7b; size: 8.5in 11.0in; margin: 0in 0in 0in 0in;
+        mso-header-margin: 0in; mso-footer-margin: 0in; mso-paper-source: 0;
+        &#x7d; div.WordSection1 &#x7b; page: WordSection1; &#x7d; table &#x7b;
+        border-collapse: collapse; border: 0; margin: 0; padding: 0;
+        vertical-align: middle; &#x7d; &lt;/style&gt; &lt;!--[if gte mso 10]&gt;
+        &lt;style&gt; /* Style Definitions */ table.MsoNormalTable
+        &#x7b;mso-style-name:"Table Normal"; mso-tstyle-rowband-size:0;
+        mso-tstyle-colband-size:0; mso-style-noshow:yes; mso-style-priority:99;
+        mso-style-parent:""; mso-padding-alt:0in 0pt 0in 0pt;
+        mso-para-margin:0in; mso-para-margin-bottom:.0pt;
+        mso-pagination:widow-orphan; font-size:10.0pt; border: 0;
+        font-family:"Times New Roman",serif;&#x7d; .ExternalClass&#x7b;
+        width:100%; &#x7d; .ExternalClass, .ExternalClass p, .ExternalClass
+        span, .ExternalClass font, .ExternalClass td, .ExternalClass div&#x7b;
+        line-height: 100%; &#x7d; table&#x7b; mso-table-lspace:0pt;
+        mso-table-rspace:0pt; border-collapse: collapse; border: 0; margin: 0;
+        padding: 0; &#x7d; table td &#x7b; border-collapse: collapse; margin: 0;
+        padding: 0; border: 0; &#x7d; img&#x7b; -ms-interpolation-mode:bicubic;
+        &#x7d; .button &#x7b; border: none !important; padding: none !important;
+        &#x7d; .cell &#x7b; width: 123px !important; &#x7d; &lt;/style&gt;
+        &lt;![endif]--&gt; &lt;!--[if gte mso 9]&gt;&lt;xml&gt;
+        &lt;o:shapedefaults v:ext="edit" spidmax="1026"/&gt;
+        &lt;/xml&gt;&lt;![endif]--&gt; &lt;!--[if gte mso 9]&gt;&lt;xml&gt;
+        &lt;o:shapelayout v:ext="edit"&gt; &lt;o:idmap v:ext="edit"
+        data="1"/&gt; &lt;/o:shapelayout&gt;&lt;/xml&gt;&lt;![endif]--&gt;
+        &lt;/head&gt; &lt;body style="margin: 0px !important; padding: 0px
+        !important;" bgcolor="#E3E3E3" lang=EN-US link="#0078D1"
+        vlink="#0078D1"&gt; &lt;custom name="opencounter" type="tracking"&gt;
+        &lt;div class=WordSection1&gt; &lt;table border=0 cellspacing=0
+        cellpadding=0 width="100%" style='width:100.0%;background:#E3E3E3;'&gt;
+        {this.props.Secs.map((section, Sindex) =>
+          this.props.Secs[Sindex].content.map((content, Cindex) =>
+            this.props.Secs[Sindex].content[Cindex].content.editorState ==
+            undefined ? null : (
+              <p>
+                &lt;tr&gt; &lt;td&gt; &lt;div align=center&gt; &lt;table
+                class="main" cellspacing=0 cellpadding=0&gt; &lt;tr&gt; &lt;td
+                width=640&gt; &lt;table border=0 cellspacing=0 cellpadding=0
+                width=100% style="background: #ffffff;"&gt; &lt;tr&gt; &lt;td
+                width=640&gt; &lt;table border=0 cellspacing=0 cellpadding=0
+                align=left width=640&gt; &lt;tr&gt; &lt;td width=640px
+                valign=middle align=left style='padding: 0px 0px 0px 0px;'&gt;
+                {convertToHTML({
+                  styleToHTML: style => {
+                    // if (style === "BOLD") {
+                    //   return <strong />;
+                    // }
+                    // if (style === "ITALIC") {
+                    //   return <em />;
+                    // }
+                    // if (style === "UNDERLINE") {
+                    //   return <span style={{ textDecoration: 'underline' }} />;
+                    // }
+                    if (style === "blue") {
+                      return <span style={{ color: "#0078D7" }} />;
+                    }
+                    if (style === "white") {
+                      return <span style={{ color: "white" }} />;
+                    }
+                  },
+                  blockToHTML: block => {
+                    console.log("block: " + block.type);
+                    if (block.type === "header-one") {
+                      return {
+                        start:
+                          '<h1 style="margin: 0px; font-size:37px; font-family: &#39;Segoe UI&#39;; color: #505050;text-align: ' +
+                          this.props.Secs[Sindex].content[Cindex].alignment +
+                          ';">',
+                        end: "</h1>",
+                        empty: ""
+                      };
+                    }
+                    if (block.type === "header-two") {
+                      return {
+                        start:
+                          '<h2 style="margin: 0px; font-size:28px; font-family: &#39;Segoe UI&#39;; color: #505050;text-align: ' +
+                          this.props.Secs[Sindex].content[Cindex].alignment +
+                          ';">',
+                        end: "</h2>",
+                        empty: ""
+                      };
+                    }
+                    if (block.type === "header-three") {
+                      return {
+                        start:
+                          '<h3 style="margin: 0px; font-size:20px; font-family: &#39;Segoe UI&#39;; color: #505050;text-align: ' +
+                          this.props.Secs[Sindex].content[Cindex].alignment +
+                          ';">',
+                        end: "</h3>",
+                        empty: ""
+                      };
+                    }
+                    if (block.type === "paragraph") {
+                      return {
+                        start:
+                          '<p style="margin: 0px; font-size:16px; font-family: &#39;Segoe UI&#39;; color: #505050;text-align: ' +
+                          this.props.Secs[Sindex].content[Cindex].alignment +
+                          ';">',
+                        end: "</p>",
+                        empty: ""
+                      };
+                    }
+                    if (block.type === "unordered-list-item") {
+                      return {
+                        start:
+                          '<li style="margin: 0px; font-size:16px font-family: &#39;Segoe UI&#39;; color: #505050;">',
+                        end: "</li>",
+                        empty: ""
+                      };
+                    }
+                    // if (block.type === "H1") {
+                    //   return { element: <h1 />, empty: <br /> };
+                    // }
+                    // if (block.type === "H2") {
+                    //   return { element: <h2 />, empty: <br /> };
+                    // }
+                    // if (block.type === "H3") {
+                    //   return { element: <h3 />, empty: <br /> };
+                    // }
+                    if (block.type === "unstyled") {
+                      return {
+                        start:
+                          '<p style="margin: 0px; font-size:16px font-family: &#x27Segoe UI&#x27; color: #505050;text-align:' +
+                          this.props.Secs[Sindex].content[Cindex].alignment +
+                          '">',
+                        end: "</p>",
+                        empty: ""
+                      };
+                    }
+                    if (block.type === "atomic") {
+                      return {
+                        start: "",
+                        end: ""
+                      };
+                    }
+                  },
+                  entityToHTML: (entity, originalText) => {
+                    console.log(entity);
+                    if (entity.type === "LINK") {
+                      return <a href={entity.data.url}>{originalText}</a>;
+                    }
+                    if (entity.type === "image") {
+                      return <img alt="" src={entity.data.src} />;
+                    }
+                    return originalText;
+                  }
+                })(
+                  this.props.Secs[Sindex].content[
+                    Cindex
+                  ].content.editorState.getCurrentContent()
+                )}
+                &lt;/td&gt; &lt;/tr&gt; &lt;/table&gt; &lt;/td&gt; &lt;/tr&gt;
+                &lt;/table&gt; &lt;/td&gt; &lt;/tr&gt; &lt;/table&gt;
+                &lt;/div&gt; &lt;/td&gt; &lt;/tr&gt;
+              </p>
+            )
+          )
+        )}
+        &lt;/table&gt; &lt;/div&gt; &lt;/body&gt; &lt;/html&gt;
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-      Secs: state.sections,
-    }
-  }
+  return {
+    Secs: state.sections
+  };
+};
 
-  
-  export default connect(mapStateToProps)(ConvertToHTML);
+export default connect(mapStateToProps)(ConvertToHTML);
