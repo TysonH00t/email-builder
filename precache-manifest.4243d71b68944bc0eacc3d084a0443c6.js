65b8dad8ae25f0ebde5ad4903c0ca22066648de3
Tyson <tyshuotari@gmail.com>
2019-09-07 21:58:13 -0700

    Updates

---
+++ precache-manifest.4243d71b68944bc0eacc3d084a0443c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e8dd26c01f55f52fa9d3f404360c26",
+    "url": "/email-builder/index.html"
+  },
+  {
+    "revision": "334a28845d1c653efeab",
+    "url": "/email-builder/static/css/2.a17ebd6e.chunk.css"
+  },
+  {
+    "revision": "ab96fdaa698db2561320",
+    "url": "/email-builder/static/css/main.d47993e5.chunk.css"
+  },
+  {
+    "revision": "334a28845d1c653efeab",
+    "url": "/email-builder/static/js/2.4fc77f6d.chunk.js"
+  },
+  {
+    "revision": "ab96fdaa698db2561320",
+    "url": "/email-builder/static/js/main.5ff78873.chunk.js"
+  },
+  {
+    "revision": "649e76c52f342279b5a1",
+    "url": "/email-builder/static/js/runtime~main.769bb8e9.js"
+  }
+]);
